66b624f8addccdfe6ec0e3a1b292cc3eaa317970
Kevin <bywarrioryt@gmail.com>
2019-12-16 04:46:06 +0100

    Arreglando el script para que saliera bien la encuesta

--- test_input.py
+++ test_input.py
@@ -1,9 +1,9 @@
 
 print ("Cual es el numero mas alto: ")
 
-primer_numero = int(input("primer_numero"))
-segundo_numero = int(input("segundo_numero"))
-tercer_numero = int(input("tercer_numero"))
+primer_numero = int(input("primer_numero: "))
+segundo_numero = int(input("segundo_numero: "))
+tercer_numero = int(input("tercer_numero: "))
 
 resultado = max(primer_numero, segundo_numero, tercer_numero)
 
