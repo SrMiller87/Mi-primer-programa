a6037d76f8492e1265870c840416fe01465bcaf3
Kevin <bywarrioryt@gmail.com>
2019-12-16 04:43:32 +0100

    Intentando utilizar bien el .format y el print

--- test_input.py
+++ test_input.py
@@ -1,6 +1,10 @@
 
+print ("Cual es el numero mas alto: ")
 
-numero_del_usuario = input("Dime un numero: ")
-print(numero_del_usuario)
+primer_numero = int(input("primer_numero"))
+segundo_numero = int(input("segundo_numero"))
+tercer_numero = int(input("tercer_numero"))
 
+resultado = max(primer_numero, segundo_numero, tercer_numero)
 
+print ("El numero mas alto es {}".format (resultado))
