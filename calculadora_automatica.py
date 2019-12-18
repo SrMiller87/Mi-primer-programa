1f5b73629ad7251d4837cc63674e668fe38a3be3
Kevin <bywarrioryt@gmail.com>
2019-12-18 05:01:28 +0100

    Intentando perfeccionar la calculadora, para darle mas opciones al usuario

--- calculadora_automatica.py
+++ calculadora_automatica.py
@@ -1,6 +1,20 @@
 
+#---------------------------------------MENU DEL PROGRAMA-------------------------------------------
+print("---Si pulsa FIN se cerrara el programa automaticamente---")
 user_action = input("Que operacion quieres realizar? [Sumar] / [Restar] / [Multiplicar] / [Dividir] : ").upper()
 
+regresar_menu = input("Quiere salir del programa? (Si) (No) : ")
+
+if regresar_menu == "Si":
+    regresar_menu = True
+    print("Saliendo...")
+elif regresar_menu == "No":
+    regresar_menu = False
+    print("Continuamos pues")
+
+if user_action == "FIN":
+    print("El programa se cerrara")
+#--------------------------------------FUNCIONES DEL PROGRAMA---------------------------------------
 while user_action == "SUMAR":
     primer_numero = int(input("Primer Numero: "))
     segundo_numero = int(input("Segundo Numero: "))
@@ -28,13 +42,5 @@ while user_action == "DIVIDIR":
     if primer_numero and segundo_numero:
         print("El resultado de la Division es: ")
         print(primer_numero / segundo_numero)
-
-
-
-
-
-
-
-
-
+#---------------------------------------VARIABLES-------------------------------------------------
 
