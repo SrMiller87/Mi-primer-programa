06bd137e87b97153cdb7382f4ea521e7e05634ae
Kevin <bywarrioryt@gmail.com>
2019-12-20 01:42:46 +0100

    Calculadora al 100% terminada :)

--- calculadora_automatica.py
+++ calculadora_automatica.py
@@ -1,37 +1,22 @@
-def menu(regresar_menu):
-    user_action = input("Que operacion quieres realizar? [Sumar] / [Restar] / [Multiplicar] / [Dividir] : ").upper()
-    accionar_menu = 0
-    while accionar_menu <= 0:
-        accionar_menu + 1
-    while accionar_menu > 0:
-        print("Volviendo al menu...")
-        return user_action
 
-while user_action == "SUMAR":
-    primer_numero = int(input("Primer Numero: "))
-    segundo_numero = int(input("Segundo Numero: "))
-    if primer_numero and segundo_numero:
-        print("El resultado de la Suma es: ")
-        print(primer_numero + segundo_numero)
+N1 = int(input("Escribe el primer numero: "))
+N2 = int(input("Escribe el segundo numero: "))
 
-while user_action == "RESTAR":
-    primer_numero = int(input("Primer Numero: "))
-    segundo_numero = int(input("Segundo Numero: "))
-    if primer_numero and segundo_numero:
-        print("El resultado de la Resta es: ")
-        print(primer_numero - segundo_numero)
+print("1. Sumar")
+print("2. Restar")
+print("3. Multiplicar")
+print("4. Dividir")
 
-while user_action == "MULTIPLICAR":
-    primer_numero = int(input("Primer Numero: "))
-    segundo_numero = int(input("Segundo Numero: "))
-    if primer_numero and segundo_numero:
-        print("El resultado de la Multiplicacion es: ")
-        print(primer_numero * segundo_numero)
+Type = int(input("Elige una opción: "))
 
-while user_action == "DIVIDIR":
-    primer_numero = int(input("Primer Numero: "))
-    segundo_numero = int(input("Segundo Numero: "))
-    if primer_numero and segundo_numero:
-        print("El resultado de la Division es: ")
-        print(primer_numero / segundo_numero)
+if Type == 1:
+    print("El resultado es {}".format(N1 + N2))
 
+if Type == 2:
+    print("El resultado es {}".format(N1 - N2))
+
+if Type == 3:
+    print("El resultado es {}".format(N1 * N2))
+
+if Type == 4:
+    print("El resultado es {}".format(N1 / N2))
